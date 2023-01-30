acaeb8d982afbc90aeb9c6799326748a52491cc1
Rachel Sanchez <rachel.sanchez@live.com>
2023-01-30 16:16:28 -0600

    routes on back end

--- server/routes/candidate.route.js
+++ server/routes/candidate.route.js
@@ -2,13 +2,12 @@ const CandidateController = require('../controllers/candidate.controller');
 const {authenticate} = require('../config/jwt.config')
 
 module.exports = (app) => {
-    app.post('/api/candidate', authenticate, CandidateController.createCandidate);
-    app.get('/api/candidate/:id', authenticate, CandidateController.findOneCandidate);
-    app.get('/api/candidates', authenticate, CandidateController.findAllCandidates);
-    app.put('/api/vote/:id', authenticate, CandidateController.updateCandidate);
+    app.post('/api/candidate', CandidateController.createCandidate);
+    app.get('/api/candidate/:id', CandidateController.findOneCandidate);
+    app.get('/api/candidates', CandidateController.findAllCandidates);
     app.put('/api/vote/:id', authenticate, CandidateController.updateCandidate);
     app.put('/edit/candidate/:id', authenticate, CandidateController.editCandidate);
     app.delete('/api/:id', authenticate, CandidateController.deleteAnExistingCandidate);
-    app.get('/api/winner', authenticate, CandidateController.findOneWinner);
+    app.get('/api/winner', CandidateController.findOneWinner);
 
 }
