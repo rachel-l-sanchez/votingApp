72635742e5bd802f23a8f45aeb15f9c0556625ec
Rachel Sanchez <rachel.sanchez@live.com>
2023-01-30 19:59:51 -0600

    login and logout

--- server/routes/candidate.route.js
+++ server/routes/candidate.route.js
@@ -2,7 +2,6 @@ const CandidateController = require("../controllers/candidate.controller");
 const { authenticate } = require("../config/jwt.config");
 
 module.exports = (app) => {
-
     app.post('/api/candidate', CandidateController.createCandidate);
     app.get('/api/candidate/:id', CandidateController.findOneCandidate);
     app.get('/api/candidates', CandidateController.findAllCandidates);
@@ -10,5 +9,4 @@ module.exports = (app) => {
     app.put('/edit/candidate/:id', CandidateController.editCandidate);
     app.delete('/api/:id', CandidateController.deleteAnExistingCandidate);
     app.get('/api/winner', CandidateController.findOneWinner);
-
 }
