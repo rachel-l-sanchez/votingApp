6adcba4d89effe4dbce87ef8e5b36e0619244127
Rachel Sanchez <rachel.sanchez@live.com>
2023-01-31 08:48:27 -0600

    fixed register component

--- ballotbox/src/components/Register.js
+++ ballotbox/src/components/Register.js
@@ -1,7 +1,6 @@
-import React from 'react'
+import React, {useState} from 'react'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
-import { useState } from 'react'
 
 const Register = ({setAdmin}) => {
   const [firstName, setFirstName]= useState("")
@@ -18,15 +17,20 @@ const Register = ({setAdmin}) => {
     const newUser = {firstName, lastName, email, password}
     console.log(newUser)
 
-    axios.post('http://localhost:8000/api/admin/register', newUser,
-    {withCredentials:true, credentials:'include'})
-      .then (res => {
+    axios.post('http://localhost:8000/api/admin/register', {
+        firstName,
+        lastName,
+        email,
+        password
+    },{withCredentials:true, credentials:'include'})
+      .then ((res) => {
         console.log("logged in admin" + res.data.admin)
-        setErrors([])
         setAdmin(res.data.admin)
         navigate('/admin')
       })
-      .catch(res => setErrors(res.response.data.errors))
+      .catch((err) => {
+        setErrors(err.response.data.errors)
+      })
       
   }
 
@@ -34,43 +38,44 @@ const Register = ({setAdmin}) => {
   return (
     <div className="bg-div">
       <h1>Register New Admin</h1>
-      <form onSubmit={()=>handleSubmit}>
+      <form onSubmit={handleSubmit}>
                 {/* first name */}
-                {errors.firstName ? <span className="accent">{errors.firstName.message}</span> : null}
                 <br/>
                 <label>
                     First Name:
-                    <input type='text' onChange={e=>setFirstName(e.target.value)}/>
+                    <input type='text' onChange={(e)=>setFirstName(e.target.value)}/>
                 </label>
                 {/* last name */}
-                {errors.lastName ? <span className="accent">{errors.lastName.message}</span> :null}
+                {errors.firstName ? <span className="accent">{errors.firstName.message} </span>: null }
                 <br/>
                 <label>
                     Last Name:
-                    <input type='text' onChange={e=>setLastName(e.target.value)}/>
+                    <input type='text' onChange={(e)=>setLastName(e.target.value)}/>
                 </label>
                 {/* email */}
-                {errors.email ? <span className="accent">{errors.email.message}</span> : null}
+                {errors.lastName ? <span className="accent">{errors.lastName.message} </span> : null}
                 <br/>
                 <label>
                     Email:
-                    <input type='text' onChange={e=>setEmail(e.target.value)}/>
+                    <input type='text' onChange={(e)=>setEmail(e.target.value)}/>
                 </label>
                 {/* password */}
-                {errors.password ? <span className="accent">{errors.password.message}</span> :null}
+                {errors.email ? <span className="accent">{errors.email.message} </span>: null}
                 <br/>
                 <label>
                     Password:
-                    <input type='password' onChange={e=>setPassword(e.target.value)}/>
+                    <input type='password' onChange={(e)=>setPassword(e.target.value)}/>
                 </label>
+                {errors.password ? <span className="accent">{errors.password.message} </span>: null}
+
                 {/* confirm email */}
-                {errors.confirmPassword ? <span className="accent">{errors.confirmPassword.message}</span> :null}
+                {errors.confirmPassword ? <span className="accent">{errors.confirmPassword.message} </span>: null }
                 <br/>
                 <label>
                     Confirm Password:
-                    <input type='password' onChange={e=>setConfirmPassword(e.target.value)}/>
+                    <input type='password' onChange={(e)=>setConfirmPassword(e.target.value)}/>
                 </label>
-                <input type='submit' value='Submit'/>
+                <button>Submit</button>
       </form>
 
 
